5b7059e18bfae708399f9b1710358d7c05671564
Maryush Soroka <maryush.soroka@skoltech.ru>
2024-11-15 19:31:02 +0300

    Add sindrets/diffview.nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,4 +45,14 @@ return {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose" },
+    init = function()
+      local map = vim.keymap.set
+      map("n", "<leader>dv", "<cmd>DiffviewOpen<CR>", { desc = "Diffview open" })
+      map("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Diffview close" })
+    end,
+    config = true,
+  }
 }
