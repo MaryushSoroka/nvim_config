a876ef1c14e359a5c71bed98f1b43154b14085d3
Maryush Soroka <maryush.soroka@skoltech.ru>
2024-11-15 20:27:40 +0300

    Add mapping to open floating diagnostic window

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>do", vim.diagnostic.open_float, { desc = "Diagnostic open floating window"})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
