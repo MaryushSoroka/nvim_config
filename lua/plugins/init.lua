e43553b0f81beb8c5fe010f130005862e809db50
Maryush Soroka <maryush.soroka@skoltech.ru>
2024-12-28 15:23:20 +0300

    Disable VenvSelect shortcut

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,14 +36,15 @@ return {
         "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", --optional
         { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
       },
-    lazy = false,
+    -- lazy = false,
+    cmd = { "VenvSelect" },
     branch = "regexp", -- This is the regexp branch, use this for the new version
     config = function()
         require("venv-selector").setup()
       end,
-    keys = {
-      { ",v", "<cmd>VenvSelect<cr>" },
-    },
+    -- keys = {
+    --   { ",v", "<cmd>VenvSelect<cr>" },
+    -- },
   },
   {
     "sindrets/diffview.nvim",
