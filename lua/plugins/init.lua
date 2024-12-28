9eafd59f1e41a97c2773c6d3b6a62140e55c8166
Maryush Soroka <sorokamario@gmail.com>
2024-12-28 15:28:40 +0300

    Disable treesitter on large files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,6 +27,13 @@ return {
   		ensure_installed = {
         "lua", "luadoc", "printf", "vim", "vimdoc", "c", "cpp"
       },
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end
   	},
   },
   {
